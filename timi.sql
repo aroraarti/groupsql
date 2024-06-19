f62cd4ec0961b90f0fe60879f103c06eefbce062
timiolomo <timiolomo@gmail.com>
2024-06-19 22:23:35 +0100

    Delete timi.sql

--- timi.sql
+++
@@ -1,7 +0,0 @@
-SELECT p.product_id, p.product_name, count(op.order_id) as total_orders from order_products 
-op
-join products p on op.product_id = p.product_id
-join aisles a on p.aisle_id = a.aisle_id
-where a.aisle = "specialty cheeses"
-GROUP by p.product_id, p.product_name
-ORDER by total_orders DESC
